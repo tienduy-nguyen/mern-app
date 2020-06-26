12666731448b729fa4098b1ee55b5f2e05355e62
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-06-26 17:57:50 +0200

    React router setup

--- dev-connector/README.md
+++ dev-connector/README.md
@@ -1,9 +1,14 @@
 # Social developer connector
 
-### Pre-install package
+## Pre-install package
 
 ```bash
 npm init
 npm i express express-validator bcryptjs config gravatar jsonwebtoken mongoose request
 npm i -D nodemon concurrently
 ```
+
+## Project
+
+- Initial components Navbar & Landing Page
+- Using react router
--- dev-connector/client/src/App.js
+++ dev-connector/client/src/App.js
@@ -1,13 +1,24 @@
 import React, { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => (
-  <Fragment>
-    <Navbar></Navbar>
-    <Landing></Landing>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar></Navbar>
+      <Route exact path='/' component={Landing}></Route>
+      <section class='container'>
+        <Switch>
+          <Route exact path='/register' component={Register}></Route>
+          <Route exact path='/login' component={Login}></Route>
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
